5a1e665db6515705626d0b9cb2306b71603c529d
James Talton <jtalton@redhat.com>
2020-07-16 12:08:59 -0400

    ts-jest enhancements

--- test/server.spec.ts
+++ test/server.spec.ts
@@ -11,15 +11,7 @@ afterAll(setupAfterAll)
 
 describe(`Service Routes`, function () {
     it(`GET / returns status 200 and hello world`, async function () {
-        await expect(request.get(`/`)).resolves.toMatchObject({
-            status: 200,
-            headers: {
-                'content-type': 'application/json',
-                'content-length': '25',
-            },
-            data: {
-                message: 'Hello World',
-            },
-        })
+        const response = await request.get(`/`)
+        expect(response.data).toMatchSnapshot()
     })
 })
